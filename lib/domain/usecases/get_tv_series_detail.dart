769f5bf72c107c13dc8c3e206ddfdcceb33c77cf
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2023-01-02 14:10:52 +0700

    refactored class name

--- lib/domain/usecases/get_tv_series_detail.dart
+++ lib/domain/usecases/get_tv_series_detail.dart
@@ -3,10 +3,10 @@ import 'package:ditonton/common/failure.dart';
 import 'package:ditonton/domain/entities/tv_series_detail.dart';
 import 'package:ditonton/domain/repositories/tv_series_repository.dart';
 
-class GetTvShowDetail {
+class GetTvSeriesDetail {
   final TvSeriesRepository repository;
 
-  GetTvShowDetail(this.repository);
+  GetTvSeriesDetail(this.repository);
 
   Future<Either<Failure, TvSeriesDetail>> execute(int id) {
     return repository.getTvSeriesDetail(id);
