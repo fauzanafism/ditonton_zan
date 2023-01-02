4b1074832e6991fd3b236f7bd543f55328b24b02
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2023-01-02 13:29:06 +0700

    created tv series repository impl

---
+++ lib/data/repositories/tv_series_repository_impl.dart
@@ -0,0 +1,158 @@
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/exception.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/data/datasources/tv_local_data_source.dart';
+import 'package:ditonton/data/datasources/tv_remote_data_source.dart';
+import 'package:ditonton/data/models/tv_series_table.dart';
+import 'package:ditonton/domain/entities/episode.dart';
+import 'package:ditonton/domain/entities/tv_series.dart';
+import 'package:ditonton/domain/entities/tv_series_detail.dart';
+import 'package:ditonton/domain/repositories/tv_series_repository.dart';
+
+class TvSeriesRepositoryImpl implements TvSeriesRepository {
+  final TvSeriesRemoteDataSource remoteDataSource;
+  final TvSeriesLocalDataSource localDataSource;
+
+  TvSeriesRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+  });
+
+  @override
+  Future<Either<Failure, List<TvSeries>>> getAiringTodayTvSeriess() async {
+    try {
+      final result = await remoteDataSource.getAiringTodayTvSeriess();
+
+      return Right(result.map((model) => model.toEntity()).toList());
+    } on ServerException {
+      return Left(ServerFailure(''));
+    } on TlsException catch (e) {
+      return Left(CommonFailure('Certificated Not Valid:\n${e.message}'));
+    }
+  }
+
+  @override
+  Future<Either<Failure, TvSeriesDetail>> getTvSeriesDetail(int id) async {
+    try {
+      final result = await remoteDataSource.getTvSeriesDetail(id);
+      return Right(result.toEntity());
+    } on ServerException {
+      return Left(ServerFailure(''));
+    } on SocketException {
+      return Left(ConnectionFailure('Failed to connect to the network'));
+    } on TlsException catch (e) {
+      return Left(CommonFailure('Certificated Not Valid:\n${e.message}'));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Episode>>> getTvSeriesSeasonEpisodes(
+      int id, int seasonNumber) async {
+    try {
+      final result =
+          await remoteDataSource.getTvSeriesSeasonEpisodes(id, seasonNumber);
+      return Right(result.map((model) => model.toEntity()).toList());
+    } on ServerException {
+      return Left(ServerFailure(''));
+    } on SocketException {
+      return Left(ConnectionFailure('Failed to connect to the network'));
+    } on TlsException catch (e) {
+      return Left(CommonFailure('Certificated Not Valid:\n${e.message}'));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<TvSeries>>> getTvSeriesRecommendations(
+      int id) async {
+    try {
+      final result = await remoteDataSource.getTvSeriesRecommendations(id);
+      return Right(result.map((model) => model.toEntity()).toList());
+    } on ServerException {
+      return Left(ServerFailure(''));
+    } on SocketException {
+      return Left(ConnectionFailure('Failed to connect to the network'));
+    } on TlsException catch (e) {
+      return Left(CommonFailure('Certificated Not Valid:\n${e.message}'));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<TvSeries>>> getPopularTvSeriess() async {
+    try {
+      final result = await remoteDataSource.getPopularTvSeriess();
+
+      return Right(result.map((model) => model.toEntity()).toList());
+    } on ServerException {
+      return Left(ServerFailure(''));
+    } on TlsException catch (e) {
+      return Left(CommonFailure('Certificated Not Valid:\n${e.message}'));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<TvSeries>>> getTopRatedTvSeriess() async {
+    try {
+      final result = await remoteDataSource.getTopRatedTvSeriess();
+
+      return Right(result.map((model) => model.toEntity()).toList());
+    } on ServerException {
+      return Left(ServerFailure(''));
+    } on TlsException catch (e) {
+      return Left(CommonFailure('Certificated Not Valid:\n${e.message}'));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<TvSeries>>> searchTvSeriess(String query) async {
+    try {
+      final result = await remoteDataSource.searchTvSeries(query);
+      return Right(result.map((model) => model.toEntity()).toList());
+    } on ServerException {
+      return Left(ServerFailure(''));
+    } on SocketException {
+      return Left(ConnectionFailure('Failed to connect to the network'));
+    } on TlsException catch (e) {
+      return Left(CommonFailure('Certificated Not Valid:\n${e.message}'));
+    }
+  }
+
+  @override
+  Future<Either<Failure, String>> saveWatchlistTvSeries(
+      TvSeriesDetail tvSeries) async {
+    try {
+      final result = await localDataSource
+          .insertWatchlistTvSeries(TvSeriesTable.fromEntity(tvSeries));
+      return Right(result);
+    } on DatabaseException catch (e) {
+      return Left(DatabaseFailure(e.message));
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<Either<Failure, String>> removeWatchlistTvSeries(
+      TvSeriesDetail tvSeries) async {
+    try {
+      final result = await localDataSource
+          .removeWatchlistTvSeries(TvSeriesTable.fromEntity(tvSeries));
+      return Right(result);
+    } on DatabaseException catch (e) {
+      return Left(DatabaseFailure(e.message));
+    }
+  }
+
+  @override
+  Future<bool> isAddedToWatchlistTvSeries(int id) async {
+    final result = await localDataSource.getTvSeriesById(id);
+    return result != null;
+  }
+
+  @override
+  Future<Either<Failure, List<TvSeries>>> getWatchlistTvSeriess() async {
+    final result = await localDataSource.getWatchlistTvSeries();
+    return Right(result.map((data) => data.toEntity()).toList());
+  }
+}
