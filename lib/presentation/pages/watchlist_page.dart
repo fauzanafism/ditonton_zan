ba10fefe6fb4e258be9668449dfcd16b2fce8b04
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2023-01-04 10:17:34 +0700

    created watchlist page that show movie and tv

---
+++ lib/presentation/pages/watchlist_page.dart
@@ -0,0 +1,115 @@
+import 'package:ditonton/common/constants.dart';
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/common/utils.dart';
+import 'package:ditonton/presentation/provider/watchlist_movie_notifier.dart';
+import 'package:ditonton/presentation/provider/watchlist_tv_series_notifier.dart';
+import 'package:ditonton/presentation/widgets/movie_card_list.dart';
+import 'package:ditonton/presentation/widgets/movie_list.dart';
+import 'package:ditonton/presentation/widgets/tv_series_list.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class WatchlistPage extends StatefulWidget {
+  static const ROUTE_NAME = '/watchlist';
+
+  @override
+  _WatchlistPageState createState() => _WatchlistPageState();
+}
+
+class _WatchlistPageState extends State<WatchlistPage> with RouteAware {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() =>
+        Provider.of<WatchlistMovieNotifier>(context, listen: false)
+            .fetchWatchlistMovies());
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    routeObserver.subscribe(this, ModalRoute.of(context)!);
+  }
+
+  void didPopNext() {
+    Provider.of<WatchlistMovieNotifier>(context, listen: false)
+        .fetchWatchlistMovies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Watchlist'),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              _buildSubHeading(
+                title: 'Movies',
+                onTap: () =>
+                    Navigator.pushNamed(context, WatchlistPage.ROUTE_NAME),
+              ),
+              Consumer<WatchlistMovieNotifier>(builder: (context, data, child) {
+                final state = data.watchlistState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return MovieList(data.watchlistMovies);
+                } else {
+                  return Text('Failed');
+                }
+              }),
+              _buildSubHeading(
+                  title: 'TV Series',
+                  // onTap: () =>
+                  //     Navigator.pushNamed(context, TopRatedMoviesPage.ROUTE_NAME),
+                  onTap: () {}),
+              Consumer<WatchlistTvSeriesNotifier>(
+                  builder: (context, data, child) {
+                final state = data.watchlistState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return TvSeriesList(data.watchlistTvSeries);
+                } else {
+                  return Text('Failed');
+                }
+              }),
+            ],
+          )),
+    );
+  }
+
+  Row _buildSubHeading({required String title, required Function() onTap}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: kHeading6,
+        ),
+        InkWell(
+          onTap: onTap,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [Text('See More'), Icon(Icons.arrow_forward_ios)],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    routeObserver.unsubscribe(this);
+    super.dispose();
+  }
+}
