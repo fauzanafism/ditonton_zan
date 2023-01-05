ac2b79fa15e0d9822c141223b1045a0aadfa3c01
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2023-01-05 10:03:42 +0700

    created dummy data for tv series

--- test/dummy_data/dummy_objects.dart
+++ test/dummy_data/dummy_objects.dart
@@ -1,7 +1,11 @@
 import 'package:ditonton/data/models/movie_table.dart';
+import 'package:ditonton/data/models/tv_series_table.dart';
 import 'package:ditonton/domain/entities/genre.dart';
 import 'package:ditonton/domain/entities/movie.dart';
 import 'package:ditonton/domain/entities/movie_detail.dart';
+import 'package:ditonton/domain/entities/season.dart';
+import 'package:ditonton/domain/entities/tv_series.dart';
+import 'package:ditonton/domain/entities/tv_series_detail.dart';
 
 final testMovie = Movie(
   adult: false,
@@ -56,4 +60,69 @@ final testMovieMap = {
   'overview': 'overview',
   'posterPath': 'posterPath',
   'title': 'title',
+};
+
+//
+final testTvSeries = TvSeries(
+    backdropPath: '/tE6dWq9neq2IPSc6kJQdxyMrl7w.jpg',
+    firstAirDate: '2022-08-15',
+    genreIds: [10759, 10765, 18],
+    id: 121750,
+    name: 'Darna',
+    originalName: 'Darna',
+    overview:
+        "When fragments of a green crystal scatter in the city and turn people into destructive monsters, Narda embraces her destiny as Darna—the mighty protector of the powerful stone from Planet Marte.",
+    popularity: 909.96,
+    posterPath: "/CFOce6pbb3FRNaBaVdvNsCv5kR.jpg",
+    voteAverage: 4.8,
+    voteCount: 12);
+
+final testTvSeriesList = [testTvSeries];
+
+final testTvSeriesDetail = TvSeriesDetail(
+    backdropPath: "/hoVuI69nygLQBJ4FqgRKnukDeKt.jpg",
+    firstAirDate: "2022-12-25",
+    genres: [
+      Genre(id: 10759, name: "Action & Adventure"),
+      Genre(id: 10765, name: "Sci-Fi & Fantasy")
+    ],
+    id: 106541,
+    name: "The Witcher: Blood Origin",
+    originalName: "The Witcher: Blood Origin",
+    overview:
+        "More than a thousand years before the world of The Witcher, seven outcasts in the elven world unite in a blood quest against an unstoppable power.",
+    posterPath: "/vpfJK9F0UJNcAIIeC42oJyKMnZQ.jpg",
+    seasons: [
+      Season(
+          airDate: "2022-12-25",
+          episodeCount: 4,
+          id: 157411,
+          name: "Limited Series",
+          overview: "",
+          posterPath: "/49WEpzSvhz2KoPNblU3FT2gVbzP.jpg",
+          seasonNumber: 1)
+    ],
+    tagline: "Every story has a beginning.",
+    voteAverage: 6.465,
+    voteCount: 185);
+
+final testWatchlistTvSeries = TvSeries.watchlist(
+  id: 1,
+  name: 'title',
+  posterPath: 'posterPath',
+  overview: 'overview',
+);
+
+final testTvSeriesTable = TvSeriesTable(
+  id: 1,
+  name: 'title',
+  posterPath: 'posterPath',
+  overview: 'overview',
+);
+
+final testTvSeriesMap = {
+  'id': 1,
+  'overview': 'overview',
+  'posterPath': 'posterPath',
+  'name': 'title',
 };
