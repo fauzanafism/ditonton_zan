7195917f0caae6690d784fcb11c7fd520a9c102e
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2023-01-12 11:51:48 +0700

    movie detail bloc test worked

--- test/presentation/bloc/movie_detail_bloc_test.dart
+++ test/presentation/bloc/movie_detail_bloc_test.dart
@@ -2,6 +2,7 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:dartz/dartz.dart';
 import 'package:ditonton/common/failure.dart';
 import 'package:ditonton/domain/usecases/get_movie_detail.dart';
+import 'package:ditonton/presentation/bloc/movie_detail_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -11,46 +12,46 @@ class MockGetMovieDetail extends Mock implements GetMovieDetail {}
 
 void main() {
   late MockGetMovieDetail mockGetMovieDetail;
-  late DetailMovieBloc detailMovieBloc;
+  late MovieDetailBloc movieDetailBloc;
 
   setUp(() {
     mockGetMovieDetail = MockGetMovieDetail();
-    detailMovieBloc = DetailMovieBloc(getMovieDetail: mockGetMovieDetail);
+    movieDetailBloc = MovieDetailBloc(getMovieDetail: mockGetMovieDetail);
   });
 
   const tId = 1;
 
   group('Movie Detail Bloc:', () {
     test('initialState should be Empty', () {
-      expect(detailMovieBloc.state, DetailMovieEmptyState());
+      expect(movieDetailBloc.state, MovieDetailEmptyState());
     });
 
-    blocTest<DetailMovieBloc, DetailMovieState>(
+    blocTest<MovieDetailBloc, MovieDetailState>(
       'should emit [Loading, HasData] when success',
       build: () {
         when(() => mockGetMovieDetail.execute(tId))
-            .thenAnswer((_) async => const Right(testMovieDetail));
-        return detailMovieBloc;
+            .thenAnswer((_) async => Right(testMovieDetail));
+        return movieDetailBloc;
       },
-      act: (bloc) => bloc.add(const FetchNowDetailMovie(id: tId)),
+      act: (bloc) => bloc.add(FetchMovieDetail(id: tId)),
       expect: () => [
-        DetailMovieLoadingState(),
-        const DetailMovieHasDataState(result: testMovieDetail)
+        MovieDetailLoadingState(),
+        MovieDetailHasDataState(result: testMovieDetail)
       ],
       verify: (bloc) => verify(() => mockGetMovieDetail.execute(tId)),
     );
 
-    blocTest<DetailMovieBloc, DetailMovieState>(
+    blocTest<MovieDetailBloc, MovieDetailState>(
       'should emit [Loading, Error] when get data is unsuccessful',
       build: () {
-        when(() => mockGetMovieDetail.execute(tId)).thenAnswer(
-            (_) async => const Left(ServerFailure('Server Failure')));
-        return detailMovieBloc;
+        when(() => mockGetMovieDetail.execute(tId))
+            .thenAnswer((_) async => Left(ServerFailure('Server Failure')));
+        return movieDetailBloc;
       },
-      act: (bloc) => bloc.add(const FetchNowDetailMovie(id: tId)),
+      act: (bloc) => bloc.add(FetchMovieDetail(id: tId)),
       expect: () => [
-        DetailMovieLoadingState(),
-        const DetailMovieErrorState(message: 'Server Failure'),
+        MovieDetailLoadingState(),
+        MovieDetailErrorState('Server Failure'),
       ],
       verify: (bloc) => verify(() => mockGetMovieDetail.execute(tId)),
     );
