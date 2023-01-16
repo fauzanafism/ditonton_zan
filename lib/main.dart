d1584ec65022eb8dc7498a8547bca98d73ac8fcc
fauzanafism <fauzan137@gmail.com>
2023-01-16 14:40:37 +0700

    added ssl pinning

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ditonton/common/constants.dart';
+import 'package:ditonton/common/ssl_pinning.dart';
 import 'package:ditonton/common/utils.dart';
 import 'package:ditonton/presentation/pages/about_page.dart';
 import 'package:ditonton/presentation/pages/airing_today_page.dart';
@@ -31,7 +32,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ditonton/injection.dart' as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SslPinning.init();
   di.init();
   runApp(MyApp());
 }
